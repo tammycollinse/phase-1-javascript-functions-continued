d238838675b526ff135b60ef453401c9767ea0fa
Tammy <tammycollinse@gmail.com>
2022-02-23 10:52:56 -0500

    commit tree cccfc168072396fb6757eee32b2c3f5248e26df3

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+//function declaration
+
+// function razzle(){
+//     console.log("You have bee razzled!");
+// }
+
+// razzle(); //calling the function
+
+// function razzle(lawyer = "Billy", target = "'em"){ //This is a function that is beinig passed arguments
+//   console.log(`${lawyer} razzle-dazzles ${target}!`);
+// }
+
+// razzle("Methuselah", "T'Challah");
+
+function saturdayFun(activity = "roller-skate" ){
+ let message = `This Saturday, I want to ${activity}!`;
+ console.log(message);
+//  if (activity != true ){
+//      activity = "roller-skate"; // why does it keep returning an undefined??? instead of assigning the "roller-skate" string?
+//      console.log(`This Saturday, I want to ${activity}!`);
+//  }
+ return message;
+}
+saturdayFun("bathe my dog");
+//-----------------
+
+function mondayWork(activityMon = "go to the office"){
+return `This Monday, I will ${activityMon}.`;
+}
+
+mondayWork("work from home");
